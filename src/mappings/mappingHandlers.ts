a1bc351be3a5e15e4824f39ab0e4c74a4750be27
valentunn <70131744+valentunn@users.noreply.github.com>
2022-12-21 14:53:28 +0700

    Feat/delegations (#1)
    * Delegations
    
    * Delete delegate if last delegation is removed
    
    * Count delegator only once per delegate
    
    * Refactoring
    
    * Code style
    
    * Fix typo

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,24 +1,139 @@
 import {
-  SubstrateExtrinsic,
-  SubstrateEvent,
-  SubstrateBlock,
+    SubstrateExtrinsic,
+    SubstrateEvent,
+    SubstrateBlock,
 } from "@subql/types";
-import { Delegate, DelegateAggregate, Delegation } from "../types";
-import { Balance } from "@polkadot/types/interfaces";
+import {Delegate, Delegation} from "../types";
+import {Big} from "big.js"
+import {INumber} from "@polkadot/types-codec/types/interfaces";
+import {Codec} from "@polkadot/types-codec/types/codec";
 
 export async function handleDelegate(extrinsic: SubstrateExtrinsic): Promise<void> {
-  const sender = extrinsic.extrinsic.signer
-  const [trackId, to, conviction, amount] = extrinsic.extrinsic.args
+    const sender = extrinsic.extrinsic.signer
+    const [trackId, to, conviction, amount] = extrinsic.extrinsic.args
 
-  // const delegateAccountIdHex = accountId.toHex()
-  //
-  // const existingRecord = await Delegate.getByAccountId(delegateAccountIdHex)
-  //
-  // if (existingRecord != undefined) {
-  //   existingRecord.delegationsId.push(sender.toHex())
-  //   existingRecord.aggregate.delegators += 1
-  //   existingRecord.aggregate.delegatorVotes += conviction
-  // }
+    const delegateAddress = to.toString()
+    const delegatorAddress = sender.toString()
+    const delegatorVotes = convictionVotes(conviction.toString(), amount.toString())
+
+    let delegate = await Delegate.get(delegateAddress)
+    if (delegate == undefined) {
+        delegate = Delegate.create({
+            id: delegateAddress,
+            accountId: delegateAddress,
+            aggregate: {
+                delegatorVotes: "0",
+                delegators: 0
+            }
+        })
+    }
+
+    const currentDelegateVotes = Big(delegate.aggregate.delegatorVotes)
+    const newDelegateVotes = currentDelegateVotes.plus(delegatorVotes)
+
+    delegate.aggregate.delegatorVotes = newDelegateVotes.toFixed()
+
+    const otherDelegatorDelegations = await Delegation.getByDelegator(delegatorAddress)
+    const isFirstDelegationToThisDelegate = otherDelegatorDelegations
+        .find((delegation) => delegation.delegateId == delegateAddress) == undefined
+
+    if (isFirstDelegationToThisDelegate) {
+        delegate.aggregate.delegators += 1
+    }
+
+    const delegation = Delegation.create({
+        id: createDelegationId(trackId.toString(), delegatorAddress),
+        delegateId: delegateAddress,
+        delegator: delegatorAddress,
+        delegation: {
+            conviction: conviction.toString(),
+            amount: amount.toString()
+        },
+        trackId: requireNumber(trackId).toNumber()
+    })
+
+    await delegation.save()
+    await delegate.save()
 }
 
+export async function handleUndelegate(extrinsic: SubstrateExtrinsic): Promise<void> {
+    const sender = extrinsic.extrinsic.signer
+    const [trackId] = extrinsic.extrinsic.args
 
+    const delegatorAddress = sender.toString()
+    const delegationId = createDelegationId(trackId.toString(), delegatorAddress)
+
+    const delegation = await Delegation.get(delegationId)
+    if (delegation == undefined) return
+
+    await Delegation.remove(delegationId)
+
+    const delegate = await Delegate.get(delegation.delegateId)
+    if (delegate == undefined) return
+
+    const currentDelegateVotes = Big(delegate.aggregate.delegatorVotes)
+    const removedVotes = convictionVotes(delegation.delegation.conviction, delegation.delegation.amount)
+    const newDelegatorVotes = currentDelegateVotes.minus(removedVotes)
+
+    const otherDelegatorDelegations = await Delegation.getByDelegator(delegatorAddress)
+    // we have already removed delegation from db above so here the list should be empty in case it was the last one
+    const wasLastDelegationToThisDelegate = otherDelegatorDelegations
+        .find((delegation) => delegation.delegateId == delegate.accountId) == undefined
+
+    if (wasLastDelegationToThisDelegate) {
+        delegate.aggregate.delegators -= 1
+    }
+    delegate.aggregate.delegatorVotes = newDelegatorVotes.toFixed()
+
+    if (delegate.aggregate.delegators == 0) {
+        await Delegate.remove(delegation.delegateId)
+    } else {
+        await delegate.save()
+    }
+}
+
+function createDelegationId(trackId: string, delegatorAccountIdHex: string): string {
+    return delegatorAccountIdHex + ":" + trackId
+}
+
+function convictionMultiplier(conviction: String): Big {
+    let multiplier: number
+
+    switch (conviction) {
+        case "None":
+            multiplier = 0.1
+            break
+        case "Locked1x":
+            multiplier = 1
+            break
+        case "Locked2x":
+            multiplier = 2
+            break
+        case "Locked3x":
+            multiplier = 3
+            break
+        case "Locked4x":
+            multiplier = 4
+            break
+        case "Locked5x":
+            multiplier = 5
+            break
+        case "Locked6x":
+            multiplier = 6
+            break
+        default:
+            throw Error(`Unknown conviction type: ${conviction}`)
+    }
+
+    return Big(multiplier)
+}
+
+function requireNumber(codec: Codec): INumber {
+    return codec as INumber
+}
+
+function convictionVotes(conviction: string, votes: string): Big {
+    const votesBigDecimal = Big(votes);
+
+    return votesBigDecimal.mul(convictionMultiplier((conviction)))
+}
