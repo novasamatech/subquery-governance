286f300354f1684ce4cb713e1c18ca6beda44131
valentun <vallentunn@gmail.com>
2023-02-22 12:12:27 +0700

    Unbounded voting queires

--- src/mappings/common.ts
+++ src/mappings/common.ts
@@ -4,3 +4,5 @@ import { GenericEventData } from '@polkadot/types';
 export function getEventData(event: SubstrateEvent): GenericEventData {
     return event.event.data as GenericEventData
 }
+
+export const unboundedQueryOptions = { limit: 1_000_000 }
--- src/mappings/voting.ts
+++ src/mappings/voting.ts
@@ -18,6 +18,10 @@ import {CallBase} from "@polkadot/types/types/calls";
 import {AnyTuple} from "@polkadot/types/types/codec";
 import {Address} from "@polkadot/types/interfaces/runtime/types";
 import {getDelegateId} from "./delegate";
+import {unboundedQueryOptions} from "./common";
+import {DelegationProps} from "../types/models/Delegation";
+import {CastingVotingProps} from "../types/models/CastingVoting";
+import {DelegatorVotingProps} from "../types/models/DelegatorVoting";
 
 export async function handleVoteHandler(extrinsic: SubstrateExtrinsic): Promise<void> {
 	const callOrigin = extrinsic.extrinsic.signer
@@ -57,7 +61,7 @@ export async function handleRemoveVote(call: CallBase<AnyTuple>, callOrigin: str
 }
 
 export async function addDelegatorActiveVotings(delegate: string, delegator: string, trackId: number, vote: ConvictionVote): Promise<void> {
-	const votings = await CastingVoting.getByVoter(delegate)
+	const votings = await getCastingVotingByVoter(delegate)
 	const allTrackReferendums = await getAllActiveReferendums(trackId)
 
 	const targetVotings = votings.filter(voting => {
@@ -80,7 +84,7 @@ export async function addDelegatorActiveVotings(delegate: string, delegator: str
 }
 
 export async function removeDelegatorActiveVotings(delegate: string, delegator: string, trackId: number): Promise<void> {
-	const votings = await CastingVoting.getByVoter(delegate)
+	const votings = await getCastingVotingByVoter(delegate)
 	const allTrackReferendums = await getAllActiveReferendums(trackId)
 
 	const targetVotings = votings.filter(voting => {
@@ -168,7 +172,7 @@ async function updateVoting(voting: CastingVoting, accountVote: AccountVote, blo
 
 async function addDelegatorVotings(parentVotingId: string, delegateId: string, trackId: number): Promise<void> {
 	/// store's interface doesn't allow to query by two field so we query by voter and then filter by track id
-	const allDelegations = await Delegation.getByDelegateId(delegateId)
+	const allDelegations = await getDelegationByDelegateId(delegateId)
 
 	logger.info(`Delegations of ${delegateId}: ${allDelegations.length}`)
 
@@ -192,13 +196,31 @@ async function addDelegatorVotings(parentVotingId: string, delegateId: string, t
 }
 
 async function clearDelegatorVotings(parentVotingId: string): Promise<void> {
-	const allVotings = await DelegatorVoting.getByParentId(parentVotingId)
+	const allVotings = await getDelegatorVotingByParentId(parentVotingId)
 
 	/// store interface doesn't allow bulk removal so do it one by one
 	for(var voting of allVotings) {
 		logger.info(`Clear delegate voting: ${parentVotingId}`)
 		await DelegatorVoting.remove(voting.id)
 	}
+}
+
+async function getDelegationByDelegateId(delegateId: string): Promise<Delegation[] | undefined> {
+	const records = await store.getByField('Delegation', 'delegateId', delegateId, unboundedQueryOptions);
+
+	return records.map(record => this.create(record as DelegationProps));
+}
+
+async function getCastingVotingByVoter(voter: string): Promise<CastingVoting[] | undefined>{
+	const records = await store.getByField('CastingVoting', 'voter', voter, unboundedQueryOptions);
+
+	return records.map(record => this.create(record as CastingVotingProps));
+}
+
+async function getDelegatorVotingByParentId(parentId: string): Promise<Delegation[] | undefined> {
+	const records = await store.getByField('DelegatorVoting', 'parentId', parentId, unboundedQueryOptions);
+
+	return records.map(record => this.create(record as DelegatorVotingProps));
 }
 
 function extractStandardVote(accountVote: AccountVote): StandardVote {
