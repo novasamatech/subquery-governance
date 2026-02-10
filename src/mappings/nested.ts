5111b397dc778d36dceb46ae0999b07a711d16e2
kogeler <25884155+kogeler@users.noreply.github.com>
2026-02-10 10:32:45 +0200

    fix

--- src/mappings/nested.ts
+++ src/mappings/nested.ts
@@ -1,8 +1,6 @@
 import { SubstrateExtrinsic } from "@subql/types";
-import { cryptoWaitReady } from "@polkadot/util-crypto";
 import { visitNestedCalls } from "../utilities/CallWalk";
 
 export async function handleNestedCalls(extrinsic: SubstrateExtrinsic): Promise<void> {
-  await cryptoWaitReady();
   await visitNestedCalls(extrinsic);
 }
